debd4be532b8e979ad6af0de386ff6329a56b9ae
jeka-vasilyev <zheka_v@inbox.ru>
2026-01-04 23:29:45 +0300

    Add backend configuration for S3 in Terraform setup

--- main.tf
+++ main.tf
@@ -1,3 +1,20 @@
+terraform {
+  backend "s3" {
+    bucket = "terraform-state"
+    key    = "dev/terraform.tfstate"
+    region = "us-east-1"
+
+    endpoints = {
+      s3 = "http://localhost:4566"
+    }
+    use_path_style              = true
+    skip_credentials_validation = true
+    skip_metadata_api_check     = true
+    skip_region_validation      = true
+    skip_requesting_account_id  = true
+  }
+}
+
 resource "aws_s3_bucket" "project_bucket" {
   bucket = var.project_name
   tags = {
